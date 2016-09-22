7736b77695ebbcd18b4a4f952e059e8c6d4db098
starship9 <nishanksaini10@gmail.com>
2016-09-23 03:53:28 +0530

    Added partial sports window script

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,50 @@
 /* Window Draggable */
-$( ".window" ).draggable({ cancel: ".window-content,input,textarea" });
-$( ".window" ).toggleClass('hidden');
+$(".window").draggable({
+    cancel: ".window-content,input,textarea"
+});
+$(".window").toggleClass('hidden');
 
 
-function attach_button_with_window(button, container){
+function attach_button_with_window(button, container) {
 
-	var $button = $(button);
-  var $container = $(container);
+    var $button = $(button);
+    var $container = $(container);
 
-	var buttonHeight = $button.height();
-  var buttonWidth = $button.width();
-  
-  $(container).find(".close").on('click', function(){
-  	$(container).toggleClass('hidden');
-  });
+    var buttonHeight = $button.height();
+    var buttonWidth = $button.width();
 
-  $button.on('click', function () {
-    
-    var buttonOffset = $button.offset();
-    var containerOffset = $container.offset();
-    
-    var diffX = containerOffset.left - buttonOffset.left - buttonWidth*0.5;
-    var diffY = containerOffset.top - buttonOffset.top - buttonHeight*0.5;
-    
-    var origin = -diffX + 'px ' + -diffY + 'px';
-    
-    console.log(origin);
-    
-    $container
-        .css({
-          transformOrigin: origin  
-        })
-        .toggleClass('hidden');
-        
-  });
+    $(container).find(".close").on('click', function() {
+        $(container).toggleClass('hidden');
+    });
+
+    $button.on('click', function() {
+
+        var buttonOffset = $button.offset();
+        var containerOffset = $container.offset();
+
+        var diffX = containerOffset.left - buttonOffset.left - buttonWidth * 0.5;
+        var diffY = containerOffset.top - buttonOffset.top - buttonHeight * 0.5;
+
+        var origin = -diffX + 'px ' + -diffY + 'px';
+
+        console.log(origin);
+
+        $container
+            .css({
+                transformOrigin: origin
+            })
+            .toggleClass('hidden');
+
+    });
 
 }
 
-$(function () {
-  
-	attach_button_with_window("#cultural_icon", "#cultural");
-	attach_button_with_window("#tech_icon", "#tech");
-	attach_button_with_window("#sports_icon", "#sports");
-	attach_button_with_window("#about_icon", "#about");
-    
-  
+$(function() {
+
+    attach_button_with_window("#cultural_icon", "#cultural");
+    attach_button_with_window("#tech_icon", "#tech");
+    attach_button_with_window("#sports_icon", "#sports");
+    attach_button_with_window("#about_icon", "#about");
+
+
 });
